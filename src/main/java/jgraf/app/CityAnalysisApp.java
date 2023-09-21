1fc29e7706894c4f81a2feb5e3e921535f791914
Son H. Nguyen <son.nguyen@tum.de>
2023-09-22 00:22:54 +0200

    Update config for CityGML 3.0

--- src/main/java/jgraf/app/CityAnalysisApp.java
+++ src/main/java/jgraf/app/CityAnalysisApp.java
@@ -18,6 +18,16 @@ public class CityAnalysisApp {
         cityGMLNeo4jDB.remainOpen();
     }
 
+    private static void analyseCityGMLV3() throws InterruptedException {
+        CityGMLNeo4jDBV3 cityGMLNeo4jDB
+                = new CityGMLNeo4jDBV3(new CityGMLNeo4jDBConfig("config/citygmlv3.conf"));
+        cityGMLNeo4jDB.open();
+        cityGMLNeo4jDB.mapFromConfig(); // Only mapping supports CityGML 3.0
+        cityGMLNeo4jDB.summarize();
+        cityGMLNeo4jDB.close();
+        //cityGMLNeo4jDB.remainOpen();
+    }
+
     public static void main(String[] args) throws InterruptedException {
         analyseCityGMLV2();
     }

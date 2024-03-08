b0eba8eda0e5dfdb01fca604637f38796948283a
Son H. Nguyen <son.nguyen@tum.de>
2024-03-08 05:18:39 +0100

    Set Xlint

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 docker build --no-cache -t citymodel-compare .
 docker run -it --rm -p7474:7474 -p7687:7687 citymodel-compare
 
+# Run detached with name and tail logs
+docker run -d --name citymodel-compare -p7474:7474 -p7687:7687 citymodel-compare ; docker logs --tail 50 -f citymodel-compare
+
 # For production, it is recommended to build the JAR and include it in the ENTRYPOINT of the Dockerfile
 # The JAR file is located in build/libs
 # .\gradlew shadowJar
